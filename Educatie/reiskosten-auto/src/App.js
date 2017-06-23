63830667173664649c814137e500a9c972bc1f56
McChubby <jaapruankok@hotmail.com>
2017-06-23 14:41:23 +0200

    Made a small start to the login page for the reiskosten-auto app

--- Educatie/reiskosten-auto/src/App.js
+++ Educatie/reiskosten-auto/src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function closeNav(){
+  console.log('Boom baby!');
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      // <div className="App">
+      //   <div className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h2>Welcome to React</h2>
+      //   </div>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <div className="App-container">
+        <div className="App-navbar">
+          <div className="App-navbar-toggle" onClick={closeNav}>X</div>
+          navbar
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+          content
+        </div>
       </div>
     );
   }
