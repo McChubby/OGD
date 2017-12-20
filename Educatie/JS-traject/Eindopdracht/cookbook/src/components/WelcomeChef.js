d854691aaa65ca8b01ea84b40ca0603b32b413a5
McChubby (Ruan) <jaapruankok@gmail.com>
2017-12-20 18:38:03 +0100

    Fixed the router issues

--- Educatie/JS-traject/Eindopdracht/cookbook/src/components/WelcomeChef.js
+++ Educatie/JS-traject/Eindopdracht/cookbook/src/components/WelcomeChef.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class WelcomeChef extends React.Component {
   render(){
--- Educatie/JS-traject/Eindopdracht/cookbook/src/index.js
+++ Educatie/JS-traject/Eindopdracht/cookbook/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router'; 
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom'; 
 
 // Imported Components
 import App from './components/App';
@@ -11,13 +11,19 @@ import './css/index.css';
 
 const Root = () => {
   return(
-    <BrowserRouter>
+    // <Match> and <Miss> were components in the alpha release of React Router v4.
+    // In the beta, <Match> has been renamed <Route> (and its props have changed so that  pattern is now path and exactly is exact). The <Miss> component was removed entirely. Instead you should use a <Switch> statement, which will only render the first <Route> (or <Redirect>) that is matched. You can add a pathless component as the last child of the <Switch>'s routes and it will render when none of the preceding <Route>s match.
+    // You can check out the API documentation for more details.
+    // https://reacttraining.com/react-router/
+    <Router>
       <div>
-        <Match exactly pattern="/" component={WelcomeChef}/>
-        <Match pattern="/CookBook/:bookId" component={App}/>
-        <Miss component={NotFound}/>
+        <Switch>
+          <Route exact path="/" component={WelcomeChef}/>
+          <Route path="/CookBook/:bookId" component={App}/>
+          <Route component={NotFound}/>
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
